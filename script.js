5b59f664d1a32597f428be39a1e61b4d1c6b060b
Dickens Leung <dickens.leung@outlook.com>
2021-02-16 15:55:39 -0800

    Updated Js files
    Commented out steps and process.

---
+++ script.js
@@ -0,0 +1,41 @@
+/* 
+1. Storing game status element here to allow to more easily use it later on
+
+*/
+
+/*
+2. Variables to track the game state throughout the game. 
+
+*/
+
+/*
+3. Using 'gameActive' to pause the game in case of an end scenario  
+
+/*
+
+
+/*
+4. 'current player' keeps track of who's turn is it
+
+*/
+
+/*
+5. Storing current game state here in the form of empty strings allow to easily track played cells 
+    and validate the game state. 
+
+*/
+
+/* 
+6. Message to display status of the game. Dynamic factors in those messages, 
+
+*/ 
+
+/* 
+7. Set inital message to let the players know whose turn it is
+*/
+
+
+/* 
+8. Adding event listeners to the actual game cells, also adding restart game button
+
+*/ 
