f152cc45dd9d406ee3d013a7043108c83a06a1e1
Dickens Leung <dickens.leung@outlook.com>
2021-02-20 16:34:59 -0800

    Updated script.js
    Added constants and variables to activate gameboard

--- script.js
+++ script.js
@@ -1,41 +1,57 @@
 /* 
-1. Storing game status element here to allow to more easily use it later on
-
-*/
-
-/*
-2. Variables to track the game state throughout the game. 
-
-*/
-
-/*
-3. Using 'gameActive' to pause the game in case of an end scenario  
-
-/*
-
-
-/*
-4. 'current player' keeps track of who's turn is it
-
-*/
-
-/*
-5. Storing current game state here in the form of empty strings allow to easily track played cells 
-    and validate the game state. 
-
-*/
+Adding event listeners to the actual game cells, also adding restart game button
+*/ 
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
 
 /* 
-6. Message to display status of the game. Dynamic factors in those messages, 
+Storing game status element here to allow to more easily use it later on
+- displaying messages for winner, loser, draw
+- display message for current player's turn
+*/
 
+const currentGameStatus = document.querySelector('.game-status');
+const messageWinner = () => `Player ${currentPlayer} has won !`;
+const messageDraw = () => `Game ends in draw !`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn !`;
+
+/*
+Variables to track the game state throughout the game. 
+   - Using 'gameActive' to pause the game in case of an end scenario
+   -'current player' keeps track of who's turn is it 
+   - Keeping track of current game state
+*/
+
+let gameActive = true;
+let currentPlayer = "X";
+let gameState =["", "", "", "", "", "", "", "", ""];
+
+/* We set the inital message to let the players know whose turn it is */
+
+currentGameStatus.innerHTML = currentPlayerTurn(); 
+
+/*
+Functions for gameboard. 
 */ 
 
-/* 
-7. Set inital message to let the players know whose turn it is
-*/
+function HandleCellPlayed(){
+
+}
+
+function handlePlayerChange(){
+
+}
+
+function handleResultValidation(){
+
+}
+
+function handleCellClick(){
+
+}
+
+function handleRestartGame(){
+    
+}
 
 
-/* 
-8. Adding event listeners to the actual game cells, also adding restart game button
-
-*/ 
